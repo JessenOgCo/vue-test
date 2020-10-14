1bf1075154b89cf0fe1f6a906958be4d0fee380c
Thomas Jessen <tj@jessenogco.dk>
2020-10-14 15:48:03 +0200

    dashboard page implemented

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> | 
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-
-@import './assets/css/styles.css';
-
-
+  @import './assets/css/styles.css';
 </style>
